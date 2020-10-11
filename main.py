96af3eac9b77e314f803194beffd567107092792
Cheese1654 <59622178+Cheese1654@users.noreply.github.com>
2020-10-11 13:38:25 -0400

    Add files via upload
    Question system generating random binary to hexadecimal questions and vice versa

--- main.py
+++ main.py
@@ -1,47 +1,51 @@
-import Button
-import pygame
-pygame.init()
+import random
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-res = (1200, 800)
-screen = pygame.display.set_mode(res)
-white = (255, 255, 255)
-color_light = (170, 170, 170)
-color_dark = (100, 100, 100)
-gold = (197, 179, 88)
-width = screen.get_width()
-height = screen.get_height()
-screen.fill(gold)
+def makeHBQuestion():
+    intValue = random.randrange(0, 511)
+    dummy1 = random.randrange(0, 511)
+    dummy2 = random.randrange(0, 511)
+    dummy3 = random.randrange(0, 511)
+
+    print("What is the hexadecimal " + hex(intValue)[2:] + " in binary representation?\n")
+    print(bin(intValue)[2:])
+    print(bin(dummy1)[2:])
+    print(bin(dummy2)[2:])
+    print(bin(dummy3)[2:])
+
+    answer = input("Select Answer\n")
+    if answer == bin(intValue)[2:]:
+        print('Correct')
+    else:
+        print('Oof')
+
+def makeBHQuestion():
+    intValue = random.randrange(0, 511)
+    dummy1 = random.randrange(0, 511)
+    dummy2 = random.randrange(0, 511)
+    dummy3 = random.randrange(0, 511)
+
+    print("What is the binary " + bin(intValue)[2:] + " in hexadecimal representation?\n")
+    print(hex(intValue)[2:])
+    print(hex(dummy1)[2:])
+    print(hex(dummy2)[2:])
+    print(hex(dummy3)[2:])
+
+    answer = input("Select Answer\n")
+    if answer == hex(intValue)[2:]:
+        print('Correct')
+    else:
+        print('Oof')
 
 
-def reDrawWinodw():
-    screen.fill(gold)
-    startButton.draw(screen)
-    optionsButton.draw(screen)
-    quitButton.draw(screen)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    makeBHQuestion()
+    makeHBQuestion()
 
 
-startButton = Button.button(color_light, (width * 0.25),  (height * 1 / 10), (width * 0.5), (height * 2 / 10), "Start")
-optionsButton = Button.button(color_light,(width * 0.25), (height * 4 / 10), (width * 0.5), (height * 2 / 10), "Options")
-quitButton = Button.button(color_light, (width * 0.25), (height * 7 / 10), (width * 0.5), (height * 2 / 10), "Quit")
-run = True
-
-while run:
-    reDrawWinodw()
-    pygame.display.update()
-    for ev in pygame.event.get():
-        pos = pygame.mouse.get_pos()
-        if ev.type == pygame.QUIT:
-            run = False
-            pygame.quit()
-            quit()
-        if ev.type == pygame.MOUSEBUTTONDOWN:
-            if startButton.isOver(pos):
-                print("clicked the start button")
-            elif optionsButton.isOver(pos):
-                print("clicked options button")
-            elif quitButton.isOver(pos):
-                print("clicked quit button")
-
-    pygame.display.update()
-
-pygame.quit()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
