f457e3f0e52fdb88e42630f88c6446d732517edd
Cheese1654 <59622178+Cheese1654@users.noreply.github.com>
2020-10-19 10:54:07 -0400

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PythonPals',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/nathanhilton/PythonPals',
+    license='',
+    author='Sheamus Cooper',
+    author_email='sheamus.cooper@ufl.edu',
+    description='First iteration of setup'
+)
