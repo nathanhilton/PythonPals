2ce3a166cd8d40e6419a12f3ac52e11e209777a9
Cheese1654 <59622178+Cheese1654@users.noreply.github.com>
2020-10-19 11:15:56 -0400

    Added openpyxl module

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from setuptools import setup
 setup(
     name='PythonPals',
     version='1.0',
-    packages=[setuptools.find_packages()],
+    packages=setuptools.find_packages(),
+    py_modules=["openpyxl"],
     url='https://github.com/nathanhilton/PythonPals',
     license='',
     author='Sheamus Cooper',
