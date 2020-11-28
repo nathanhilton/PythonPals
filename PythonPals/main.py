12b36d1b7e8737cd87cdf1684068e4a2cae76252
nathanhilton <nathanhilton24@gmail.com>
2020-11-28 15:24:46 -0500

    fixed lime background line

--- PythonPals/main.py
+++ PythonPals/main.py
@@ -315,7 +315,7 @@ def chooseAnswer(playerGroup, enemyGroup, question,level,q,A,B,C,D):
                 drawAnswers()
 
 def drawBattle(playerGroup, enemyGroup, w, h,level):
-    screen.fill(lime)
+    screen.fill((0,0,0))
     #pygame.draw.rect(screen, (0, 0, 0), (0, 0, theScreen.width, theScreen.height * 0.1))
     # bg = pygame.image.load("jungleBackground.jpg")
     drawBackground(playerGroup,enemyGroup,w,h,level)
